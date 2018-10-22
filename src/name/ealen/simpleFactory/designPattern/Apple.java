f300f3bea03704fc6252215e82295d7ce7ae51ee
EalenXie <ealenxie@qq.com>
2018-10-22 11:09:32 +0800

    新增简单工厂模式

---
+++ src/name/ealen/simpleFactory/designPattern/Apple.java
@@ -0,0 +1,11 @@
+package name.ealen.simpleFactory.designPattern;
+
+/**
+ * Created by EalenXie on 2018/10/22 9:58.
+ */
+public class Apple implements Fruit {
+    @Override
+    public void get() {
+        System.out.println("苹果");
+    }
+}
