48cd6d7b617e23e75a93294dfc479125bb72de41
EalenXie <ealenxie@qq.com>
2018-11-13 18:18:12 +0800

    新增装饰器模式

--- src/name/ealen/decorator/DecoratorMain.java
+++ src/name/ealen/decorator/DecoratorMain.java
@@ -17,11 +17,11 @@ public class DecoratorMain {
 
         Component component = new ConcreteComponent();
 
-        Decorator a = new ConcreteDecorator(component, "A");   //一次装饰
+        Component a = new ConcreteDecorator(component, "A");   //一次装饰
 
-        Decorator b = new ConcreteDecorator(a, "B");           //二次装饰(在一次装饰的基础上)
+        Component b = new ConcreteDecorator(a, "B");           //二次装饰(在一次装饰的基础上)
 
-        Decorator c = new ConcreteDecorator(b, "C");           //三次装饰(在二次装饰的基础上)
+        Component c = new ConcreteDecorator(b, "C");           //三次装饰(在二次装饰的基础上)
 
         c.operation();
 
